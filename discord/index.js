464d82060b5923afb48fbe14eb8c447688e67fb3
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-04-10 14:29:07 +0700

    Update code

--- discord/index.js
+++ discord/index.js
@@ -1,10 +1,17 @@
-const Discord = require('discord.js');
-require('dotenv').config();
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES" ,"GUILD_BANS","GUILD_EMOJIS_AND_STICKERS"] })
+const Discord = require("discord.js");
+require("dotenv").config();
+const client = new Discord.Client({
+  intents: [
+    "GUILDS",
+    "GUILD_MESSAGES",
+    "GUILD_BANS",
+    "GUILD_EMOJIS_AND_STICKERS",
+  ],
+});
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setPresence({ status: 'online' });
+  client.user.setPresence({ status: "online" });
 });
 
 client.login(process.env.DISCORD_TOKEN);

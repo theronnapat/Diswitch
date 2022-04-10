2bb7deb83f381c55e7d7d95fe67689275c880830
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-04-10 13:58:46 +0700

    Update twitch

--- src/twitch.ts
+++ src/twitch.ts
@@ -1,179 +1,42 @@
-import tmi from 'tmi.js'
-import { BOT_USERNAME , OAUTH_TOKEN, CHANNEL_NAME, BLOCKED_WORDS } from './constants'
+const tmi = require('tmi.js');
+require('dotenv').config();
 
-const options = {
-	options: { debug: true },
-	connection: {
-    reconnect: true,
-    secure: true,
-    timeout: 180000,
-    reconnectDecay: 1.4,
-    reconnectInterval: 1000,
-	},
-	identity: {
-		username: 'ronnapatp',
-		password: 'oauth:tidhx9773n0jlg0t3f3wdukwl49n5y'
-	},
-	channels: [ 'ronnapatp' ]
-}
+// Define configuration options
+const opts = {
+  identity: {
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD
+  },
+  channels: [
+    process.env.CHANNEL
+  ]
+};
 
-const client = new tmi.Client(options)
+// Create a client with our options
+const client = new tmi.client(opts);
 
-client.connect()
+// Register our event handlers (defined below)
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
 
-// events
-client.on('disconnected', (reason) => {
-  onDisconnectedHandler(reason)
-})
+// Connect to Twitch:
+client.connect();
 
-client.on('connected', (address, port) => {
-  onConnectedHandler(address, port)
-})
+// Called every time a message comes in
+function onMessageHandler ( msg:any, self:any) {
+  if (self) { return; } // Ignore messages from the bot
 
-client.on('hosted', (channel, username, viewers, autohost) => {
-  onHostedHandler(channel, username, viewers, autohost)
-})
+  // Remove whitespace from chat message
+  const commandName = msg.trim();
 
-client.on('subscription', (channel, username, method, message, userstate) => {
-  onSubscriptionHandler(channel, username, method, message, userstate)
-})
-
-client.on('raided', (channel, username, viewers) => {
-  onRaidedHandler(channel, username, viewers)
-})
-
-client.on('cheer', (channel, userstate, message) => {
-  onCheerHandler(channel, userstate, message)
-})
-
-client.on('giftpaidupgrade', (channel, username, sender, userstate) => {
-  onGiftPaidUpgradeHandler(channel, username, sender, userstate)
-})
-
-client.on('hosting', (channel, target, viewers) => {
-  onHostingHandler(channel, target, viewers)
-})
-
-client.on('reconnect', () => {
-  reconnectHandler()
-})
-
-client.on('resub', (channel, username, months, message, userstate, methods) => {
-  resubHandler(channel, username, months, message, userstate, methods)
-})
-
-client.on('subgift', (channel, username, streakMonths, recipient, methods, userstate) => {
-  subGiftHandler(channel, username, streakMonths, recipient, methods, userstate)
-})
-
-// event handlers
-
-client.on('message', (channel, userstate, message, self) => {
-  if(self) {
-    return
+  // If the command is known, let's execute it
+  if (commandName === '!ping') {
+    client.say('pong!');
+    console.log('Command : !ping Answer : pong!')
   }
-
-  if (userstate.username === BOT_USERNAME) {
-    console.log(`Not checking bot's messages.`)
-    return
-  }
-
-	if(message.toLowerCase() === '!hello') {
-    hello(channel, userstate)
-    return
-  }
-
-  onMessageHandler(channel, userstate, message, self)
-})
-
-function onMessageHandler (channel, userstate, message, self) {
-  checkTwitchChat(userstate, message, channel)
 }
 
-function onDisconnectedHandler(reason) {
-  console.log(`Disconnected: ${reason}`)
-}
-
-function onConnectedHandler(address, port) {
-  console.log(`Connected: ${address}:${port}`)
-}
-
-function onHostedHandler (channel, username, viewers, autohost) {
-  client.say(channel,
-    `Thank you @${username} for the host of ${viewers}!`
-  )
-}
-
-function onRaidedHandler(channel, username, viewers) {
-  client.say(channel,
-    `Thank you @${username} for the raid of ${viewers}!`
-  )
-}
-
-function onSubscriptionHandler(channel, username, method, message, userstate) {
-  client.say(channel,
-    `Thank you @${username} for subscribing!`
-  )
-}
-
-function onCheerHandler(channel, userstate, message)  {
-  client.say(channel,
-    `Thank you @${userstate.username} for the ${userstate.bits} bits!`
-  )
-}
-
-function onGiftPaidUpgradeHandler(channel, username, sender, userstate) {
-  client.say(channel,
-    `Thank you @${username} for continuing your gifted sub!`
-  )
-}
-
-function onHostingHandler(channel, target, viewers) {
-  client.say(channel,
-    `We are now hosting ${target} with ${viewers} viewers!`
-  )
-}
-
-function reconnectHandler () {
-  console.log('Reconnecting...')
-}
-
-function resubHandler(channel, username, months, message, userstate, methods) {
-  const cumulativeMonths = userstate['msg-param-cumulative-months']
-  client.say(channel,
-    `Thank you @${username} for the ${cumulativeMonths} sub!`
-  )
-}
-
-function subGiftHandler(channel, username, streakMonths, recipient, methods, userstate) {
-
-  client.say(channel,
-    `Thank you @${username} for gifting a sub to ${recipient}}.`
-  )
-
-  // this comes back as a boolean from twitch, disabling for now
-  // "msg-param-sender-count": false
-  // const senderCount =  ~~userstate["msg-param-sender-count"];
-  // client.say(channel,
-  //   `${username} has gifted ${senderCount} subs!`
-  // )
-}
-
-// commands
-
-function hello (channel, userstate) {
-  client.say(channel, `@${userstate.username}, heya!`)
-}
-
-function checkTwitchChat(userstate, message, channel) {
-  console.log(message)
-  message = message.toLowerCase()
-  let shouldSendMessage = false
-  shouldSendMessage = BLOCKED_WORDS.some(blockedWord => message.includes(blockedWord.toLowerCase()))
-  if (shouldSendMessage) {
-    // tell user
-    client.say(channel, `@${userstate.username}, sorry!  You message was deleted.`)
-    // delete message
-    client.deletemessage(channel, userstate.id)
-  }
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler (addr:any, port:any) {
+  console.log(`* Connected to ${addr}:${port}`);
 }

862f66bf912511e415eed981b24ac48e3b228818
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-04-10 14:52:30 +0700

    Add twitch

---
+++ twitch/index.js
@@ -0,0 +1,51 @@
+const tmi = require('tmi.js');
+require("dotenv").config();
+
+// Define configuration options
+const opts = {
+  identity: {
+    username: process.env.TWITCH_USERNAME,
+    password: process.env.TWITCH_PASSWORD
+  },
+  channels: [
+    process.env.TWITCH_CHANNEL
+  ]
+};
+
+// Create a client with our options
+const client = new tmi.client(opts);
+
+// Register our event handlers (defined below)
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
+
+// Connect to Twitch:
+client.connect();
+
+// Called every time a message comes in
+function onMessageHandler (target, context, msg, self) {
+  if (self) { return; } // Ignore messages from the bot
+
+  // Remove whitespace from chat message
+  const commandName = msg.trim();
+
+  // If the command is known, let's execute it
+  if (commandName === '!dice') {
+    const num = rollDice();
+    client.say(target, `You rolled a ${num}`);
+    console.log(`* Executed ${commandName} command`);
+  } else {
+    console.log(`* Unknown command ${commandName}`);
+  }
+}
+
+// Function called when the "dice" command is issued
+function rollDice () {
+  const sides = 6;
+  return Math.floor(Math.random() * sides) + 1;
+}
+
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler (addr, port) {
+  console.log(`* Connected to ${addr}:${port}`);
+}

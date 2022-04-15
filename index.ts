8b7a97b7ae1b0255410ca7c06edbd720d5c7b047
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-04-15 17:18:32 +0700

    Add twitch

--- index.ts
+++ index.ts
@@ -2,5 +2,7 @@ import dotenvFlow from "dotenv-flow"
 dotenvFlow.config()
 
 import discord from "./discord"
+import twitch from "./twitch"
 
-discord()
+discord();
+twitch();
---
+++ twitch.ts
@@ -0,0 +1,23 @@
+import tmi from "tmi.js"
+
+export default function twitch() {
+const client = new tmi.Client({
+  options: { debug: true, messagesLogLevel: "info" },
+  connection: {
+    reconnect: true,
+    secure: true,
+  },
+  identity: {
+    username: process.env.TWITCH_USERNAME,
+    password: process.env.TWITCH_PASSWORD,
+  },
+  channels: [`${process.env.TWITCH_CHANNEL}`],
+})
+client.connect().catch(console.error)
+client.on("message", (channel, tags, message, self) => {
+  if (self) return
+  if (message.toLowerCase() === "!hello") {
+    client.say(channel, `@${tags.username}, heya from development!`)
+  }
+})
+}

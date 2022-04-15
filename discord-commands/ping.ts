169059e24edb2df4531c878c986f499cc4bf2d78
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-04-15 17:09:03 +0700

    Add file

---
+++ discord-commands/ping.ts
@@ -0,0 +1,10 @@
+import { SlashCommandBuilder } from "@discordjs/builders"
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with Pong!"),
+  async execute(interaction) {
+    await interaction.reply("Pong!")
+  },
+}
---
+++ index.ts
@@ -0,0 +1,6 @@
+import dotenvFlow from "dotenv-flow"
+dotenvFlow.config()
+
+import discord from "./discord"
+
+discord()

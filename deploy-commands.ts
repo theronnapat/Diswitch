47341d13dcd08c2efa5ae1f602f10bae8e26fcd4
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-04-17 20:30:17 +0700

    add deploy commands

---
+++ deploy-commands.ts
@@ -0,0 +1,39 @@
+import fs from "node:fs"
+import { REST } from "@discordjs/rest"
+import { Routes } from "discord-api-types/v9"
+import dotenvFlow from "dotenv-flow"
+
+dotenvFlow.config()
+
+const clientId = process.env.DISCORD_CLIENT_ID as string
+const guildId = process.env.DISCORD_GUILD_ID as string
+const token = process.env.DISCORD_TOKEN as string
+
+// const commands = [
+//   new SlashCommandBuilder()
+//     .setName("ping")
+//     .setDescription("Replies with pong!"),
+//   new SlashCommandBuilder()
+//     .setName("server")
+//     .setDescription("Replies with server info!"),
+//   new SlashCommandBuilder()
+//     .setName("user")
+//     .setDescription("Replies with user info!"),
+// ].map((command) => command.toJSON())
+
+const commands = []
+const commandFiles = fs
+  .readdirSync("./discord-commands")
+  .filter((file) => file.endsWith(".ts"))
+
+for (const file of commandFiles) {
+  const command = require(`./discord-commands/${file}`)
+  commands.push(command.data.toJSON())
+}
+
+const rest = new REST({ version: "9" }).setToken(token)
+
+rest
+  .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+  .then(() => console.log("Successfully registered application commands."))
+  .catch(console.error)

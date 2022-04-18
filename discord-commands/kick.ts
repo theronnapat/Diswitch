14ff229c6c847f91bb76037cc350829d91702894
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-04-18 19:02:08 +0700

    manage: code

--- discord-commands/kick.ts
+++ discord-commands/kick.ts
@@ -2,12 +2,14 @@ import { SlashCommandBuilder } from "@discordjs/builders";
 
 module.exports = {
   data: new SlashCommandBuilder()
-	.setName('kick')
-	.setDescription('Kick user')
-	.addUserOption(option => option.setName('target').setDescription('Select a user').setRequired(true)),
+    .setName("kick")
+    .setDescription("Kick user")
+    .addUserOption((option) =>
+      option.setName("target").setDescription("Select a user").setRequired(true)
+    ),
   async execute(interaction: any) {
     await interaction.reply("Kick!");
-    const member = interaction.options.getMember('target');
+    const member = interaction.options.getMember("target");
     member.kick();
   },
 };

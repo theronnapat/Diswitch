ae54f68e937b95c90b36f9c8bd6b69add8a99f55
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-04-19 17:56:18 +0700

    add: permission in ban command

--- discord-commands/ban.ts
+++ discord-commands/ban.ts
@@ -1,15 +1,34 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
+import { GuildMember, MessageEmbed } from "discord.js";
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ban")
-    .setDescription("ban user")
+    .setDescription("Ban user")
     .addUserOption((option) =>
       option.setName("target").setDescription("Select a user").setRequired(true)
     ),
   async execute(interaction: any) {
-    await interaction.reply("Ban!");
+    const members = interaction.member as GuildMember;
     const member = interaction.options.getMember("target");
-    member.ban();
+
+    const baned = new MessageEmbed()
+      .setColor("#000000")
+      .setTitle(`You just ban <@${member.user.id}>!`)
+      .setTimestamp()
+      .setFooter({ text: "Diswitch" });
+    
+      const nopermission = new MessageEmbed()
+      .setColor("#000000")
+      .setTitle(`You don't have permission to ban user!`)
+      .setTimestamp()
+      .setFooter({ text: "Diswitch" });
+    
+      if (members.permissions.has("BAN_MEMBERS") == true) {
+      await member.ban();
+      await interaction.reply({ embeds: [baned] });
+    } else {
+      await interaction.reply({ embeds: [nopermission] });
+    }
   },
 };

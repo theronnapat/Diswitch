878b027d9854a453371f1f8c2f4dd8a83cce4e24
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-04-19 17:52:51 +0700

    manage: code from prettier

--- discord-commands/kick.ts
+++ discord-commands/kick.ts
@@ -11,21 +11,24 @@ module.exports = {
   async execute(interaction: any) {
     const members = interaction.member as GuildMember;
     const member = interaction.options.getMember("target");
+
     const kicked = new MessageEmbed()
-      .setColor('#000000')
+      .setColor("#000000")
       .setTitle(`You just kick <@${member.user.id}>!`)
       .setTimestamp()
-      .setFooter({ text: 'Diswitch'})
-    const nopermission = new MessageEmbed()
-      .setColor('#000000')
+      .setFooter({ text: "Diswitch" });
+    
+      const nopermission = new MessageEmbed()
+      .setColor("#000000")
       .setTitle(`You don't have permission to kick user!`)
       .setTimestamp()
-      .setFooter({ text: 'Diswitch'})
-    if (members.permissions.has("KICK_MEMBERS") == true){
+      .setFooter({ text: "Diswitch" });
+    
+      if (members.permissions.has("KICK_MEMBERS") == true) {
       await member.kick();
       await interaction.reply({ embeds: [kicked] });
     } else {
-      await interaction.reply({ embeds: [nopermission] })
+      await interaction.reply({ embeds: [nopermission] });
     }
   },
 };

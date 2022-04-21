3a992b6fd2c1dab04f43d9d1b7dd2d108f77c50f
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-04-21 11:05:49 +0700

    update: website

--- website/src/App.svelte
+++ website/src/App.svelte
@@ -1,9 +1,14 @@
 <script>
+	import Navbar from "./components/navbar.svelte"
 </script>
 
 <head>
 	<script src="https://cdn.tailwindcss.com"></script>
+	<link rel="stylesheet" href="https://unpkg.com/flowbite@1.4.2/dist/flowbite.min.css" />
+    <script src="https://unpkg.com/flowbite@1.4.2/dist/flowbite.js"></script>
 </head>
+
+<Navbar />
 
 <main>
 	<h1 class="text-center text-sky-400 text-5xl mt-10">DisWitch</h1>

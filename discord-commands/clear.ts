fcee7ea40e331fd9e4442d421ab37bb3639daab6
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-04-21 16:19:34 +0700

    add: permission in clear command

--- discord-commands/clear.ts
+++ discord-commands/clear.ts
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
-import { MessageEmbed } from "discord.js";
+import { MessageEmbed, GuildMember } from "discord.js";
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -11,16 +11,28 @@ module.exports = {
                 .setDescription("Amount of messages to delete")
                 .setRequired(true)
         ),
-    requiredPerms: ["MANAGE_MESSAGES"],
+    // requiredPerms: ["MANAGE_MESSAGES"],
     async execute(interaction: any) {
+        const members = interaction.member as GuildMember;
+        if ( members.permissions.has("MANAGE_MESSAGES") == true ){
         const deleted = await interaction.channel!.bulkDelete(
             interaction.options.getNumber("amount", true),
             true
         );
-
+        
         const successEmbed = new MessageEmbed()
-            .setColor("#fffff")
-            .setDescription(`You just deleted ${deleted.size} messages.`);
+            .setColor("#000000")
+            .setDescription(`You just deleted ${deleted.size} messages.`)
+            .setTimestamp()
+            .setFooter({ text: "Diswitch" });
         await interaction.reply({ embeds: [successEmbed], ephemeral: true });
+        } else {
+            const nopermission = new MessageEmbed()
+            .setColor("#000000")
+            .setTitle(`You don't have permission to clear message!`)
+            .setTimestamp()
+            .setFooter({ text: "Diswitch" });
+            await interaction.reply({ embeds: [nopermission], ephemeral: true });
+        }
     },
 };

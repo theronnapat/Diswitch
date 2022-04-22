38672c0b4efeb7d2ede3b1679fd294c21b6148c0
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-04-22 21:43:14 +0700

    update: timeout command

--- discord-commands/timeout.ts
+++ discord-commands/timeout.ts
@@ -10,26 +10,31 @@ module.exports = {
     )
     .addNumberOption((option) =>
       option.setName("time").setDescription("Time for timeout (minutes)").setRequired(true)
+    )
+    .addStringOption((option) => 
+      option.setName("reason").setDescription("Reason to timeout user").setRequired(false)
     ),
   async execute(interaction: any) {
     const members = interaction.member as GuildMember;
     const member = interaction.options.getMember("target");
-    const times = interaction.options.getNumber("time")
+    const times = interaction.options.getNumber("time");
+    const reason = interaction.options.getString("reason");
+    const permission = members.permissions.has("ADMINISTRATOR") == true
 
     const timouted = new MessageEmbed()
       .setColor("#000000")
-      .setDescription(`You just timeout <@${member.user.id}> for ${times} minutes!`)
+      .setDescription(`You just timeout <@${member.user.id}> for ${times} minutes with ${reason} reason!`)
       .setTimestamp()
       .setFooter({ text: "Diswitch" });
 
     const nopermission = new MessageEmbed()
       .setColor("#000000")
-      .setDescription(`You don't have permission to timeout user!`)
+      .setDescription(`You don't have permission to timeout user! <:narzelAmogus:961809766244564992>`)
       .setTimestamp()
       .setFooter({ text: "Diswitch" });
 
-    if (members.permissions.has("ADMINISTRATOR") == true) {
-      await member.timeout(times * 60 * 1000);
+    if (permission) {
+      await member.timeout(times * 60 * 1000, reason);
       await interaction.reply({ embeds: [timouted], ephemeral: true });
     } else {
       await interaction.reply({ embeds: [nopermission], ephemeral: true });

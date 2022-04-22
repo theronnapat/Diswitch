31364f98a9edce2c6d339435554d2f0bf8ffa3c5
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-04-22 16:03:03 +0700

    update: bot status

--- events/ready.ts
+++ events/ready.ts
@@ -3,5 +3,6 @@ module.exports = {
   once: true,
   execute(client: any) {
     console.log(`Ready! Logged in as ${client.user.tag}`);
+    client.user.setPresence({ activities: [{ name: `/help | I\'m in ${client.guilds.cache.size} server` }], status: 'online' });
   },
 };

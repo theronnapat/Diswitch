62909eb1039df7a90604cde69a7b8012a2f18359
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-04-24 21:19:26 +0700

    update: ping command

--- discord-commands/ping.ts
+++ discord-commands/ping.ts
@@ -1,10 +1,25 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
+import { MessageEmbed } from "discord.js"
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Replies with Pong!"),
   async execute(interaction: any) {
-    await interaction.reply("Pong!");
+      const ping = Date.now() - interaction.createdAt;
+
+      await interaction.reply({
+          content: "Pong!",
+          embeds: [
+            new MessageEmbed()
+            .setColor('WHITE')
+            .addFields([{
+              name: "Bot Latency",
+              value: `\`\`\`ini\n[ ${ping}ms ]\n\`\`\``,
+              inline: true
+            }])
+            .setTimestamp()
+        ]
+      });
   },
 };

4b895b045bb802e0889f0c467a5019efae01aef0
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-04-24 21:38:12 +0700

    add: embed creator

---
+++ discord-commands/embed.ts
@@ -0,0 +1,28 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { MessageEmbed, GuildMember } from "discord.js";
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("embed")
+    .setDescription("Create embed")
+    .addStringOption((option) =>
+      option.setName("title").setDescription("Set title").setRequired(true)
+    )
+    .addStringOption((option) =>
+      option.setName("description").setDescription("Set description").setRequired(true)
+    ),
+  async execute(interaction: any) {
+      const author = interaction.member as GuildMember;
+      const title = interaction.options.getString("title")
+      const description = interaction.options.getString("description")
+
+      const embed = new MessageEmbed()
+        .setColor("DARK_GOLD")
+        .setTitle(`${title}`)
+        .setDescription(`${description}`)
+        .setTimestamp()
+        .setFooter({ text: `Message create by ${author.user.username}`})
+    
+      await interaction.reply({ embeds: [ embed ] })
+  },
+};

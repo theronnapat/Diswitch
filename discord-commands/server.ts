ef5088df6af429725d8249ce50e9513ba8c8e20f
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-04-26 14:50:03 +0700

    add: server info command

---
+++ discord-commands/server.ts
@@ -0,0 +1,15 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { MessageEmbed } from "discord.js";
+import { server } from "../data"
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("server")
+    .setDescription("Server info!"),
+  async execute(interaction: any) {
+      const servers = new MessageEmbed()
+        .setTitle(server.name)
+
+    await interaction.reply({ embeds: [servers] })
+  },
+};

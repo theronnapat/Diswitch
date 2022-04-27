72f4a3d763511181d4f3b6c747e3bf5292627d3e
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-04-27 15:53:06 +0700

    manage: code from prettier

--- discord-commands/server.ts
+++ discord-commands/server.ts
@@ -1,15 +1,14 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { MessageEmbed } from "discord.js";
-import { server } from "../data"
+import { server } from "../data";
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("server")
     .setDescription("Server info!"),
   async execute(interaction: any) {
-      const servers = new MessageEmbed()
-        .setTitle(server.name)
+    const servers = new MessageEmbed().setTitle(server.name);
 
-    await interaction.reply({ embeds: [servers] })
+    await interaction.reply({ embeds: [servers] });
   },
 };

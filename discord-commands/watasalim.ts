444fb594f2df87898b529f00a557a50827966ee9
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-04-29 17:20:50 +0700

    add watasalim

---
+++ discord-commands/watasalim.ts
@@ -0,0 +1,27 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { MessageEmbed } from "discord.js";
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("salim")
+    .setDescription("วาทะสลิ่ม!"),
+  async execute(interaction: any) {
+    let response = await fetch(`https://watasalim.vercel.app/api/quotes/random`);
+    let data = await response.json();
+    let url = data.quote.url
+    let pathname = new URL(url).pathname;
+
+    const message = new MessageEmbed()
+        .setTitle("Awesome Salim Quotes")
+        .setDescription(`${data.quote.body}`)
+        .setFooter({ text: `watasalim.vercel.app${pathname}` })
+
+    await interaction.reply(
+      {
+          embeds: [
+              message
+          ]
+      }
+    );
+  },
+};

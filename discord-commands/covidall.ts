a74cd1a33cbed2b9e7dd36ec1927ba39375ba071
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-05-13 21:04:29 +0700

    Add covid command and manage code by prettier

--- discord-commands/covidall.ts
+++ discord-commands/covidall.ts
@@ -9,16 +9,40 @@ module.exports = {
     let response = await fetch(`https://disease.sh/v3/covid-19/all`);
     let data = await response.json();
     const message = new MessageEmbed()
-        .setTitle("Global Covid 19 Data")
-        .addFields(
-            { name: 'Total case', value: `${data.cases.toLocaleString()}` , inline: true },
-            { name: 'Total death', value: `${data.deaths.toLocaleString()}`, inline: true },
-            { name: 'Total recover', value: `${data.recovered.toLocaleString()}`, inline: true },
-            { name: 'Today case', value: `${data.todayCases.toLocaleString()}` , inline: true },
-          { name: 'Today death', value: `${data.todayDeaths.toLocaleString()}`, inline: true },
-          { name: 'Today recover', value: `${data.todayRecovered.toLocaleString()}`, inline: true },
-        )
-        .setFooter({ text: `Data from disease.sh` })
+      .setTitle("Global Covid 19 Data")
+      .addFields(
+        {
+          name: "Total case",
+          value: `${data.cases.toLocaleString()}`,
+          inline: true,
+        },
+        {
+          name: "Total death",
+          value: `${data.deaths.toLocaleString()}`,
+          inline: true,
+        },
+        {
+          name: "Total recover",
+          value: `${data.recovered.toLocaleString()}`,
+          inline: true,
+        },
+        {
+          name: "Today case",
+          value: `${data.todayCases.toLocaleString()}`,
+          inline: true,
+        },
+        {
+          name: "Today death",
+          value: `${data.todayDeaths.toLocaleString()}`,
+          inline: true,
+        },
+        {
+          name: "Today recover",
+          value: `${data.todayRecovered.toLocaleString()}`,
+          inline: true,
+        }
+      )
+      .setFooter({ text: `Data from disease.sh` });
 
     await interaction.reply({ embeds: [message] });
   },
